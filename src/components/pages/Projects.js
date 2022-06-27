3b24d5c1f19ed1240ed5b70dd5c9a545fbc2391b
rtleib <rtleib@gmail.com>
2022-06-27 01:00:42 -0500

    added more projects and fixed tags

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import quiz from "../../images/Quiz.png"
+import starwars from "../../images/Star-Wars.png"
+import radar from "../../images/radar-menu.png"
 
 export default function Projects() {
   return (
     <div>
-      <h1>Projects Page</h1>
+      <h1>Projects</h1>
       <section>
       <a href="https://rtleib.github.io/code-quiz-challenge-04/">
-      <p class="img-responsive" src={quiz} alt="picture of quiz" width="330" height="170"/>
+      <img class="img-responsive" src={quiz} alt="picture of quiz" width="330" height="170"/>
       </a>
-        <li>
-        <a href="https://rtleib.github.io/Star-Wars-Weekly-Scheduler/">
-          </a>
-          </li>
-          <li>
-          <a href="https://radar-menu.herokuapp.com/">
-            </a>
-            </li>
+      </section>
+      <section>
+      <a href="https://rtleib.github.io/Star-Wars-Weekly-Scheduler/">
+      <img class="img-responsive" src={starwars} alt="picture of star wars weekly scheduler"  width="330" height="170"/>
+      </a>
+      </section>
+      <section>
+      <a href="https://radar-menu.herokuapp.com/">
+      <img class="img-responsive" src={radar} alt="picture of radar menu"  width="330" height="170"/>
+      </a>
       </section>
     </div>
   );
